f23531b5deb3952c709431b79dc8553555fc6bd3
jamie120 <jamie120@hotmail.com>
2021-01-30 09:08:18 +0000

    Add 404 template route and functionality

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 @app.route("/")
 def home():
     try:
